147ba048f7d3d0e037b995ec28807c890615a3bc
nibusan <nibusan857@gmail.com>
2024-06-04 15:19:01 +0900

    Collider修正

--- Source/GameObject/2D/Collider/AbstractCollider.cpp
+++ Source/GameObject/2D/Collider/AbstractCollider.cpp
@@ -3,3 +3,11 @@
 AbstractCollider::AbstractCollider(void) : centerPos_(Vector2<float>()), type_(Collider_Info::TYPE::NONE) {}
 
 AbstractCollider::AbstractCollider(const Vector2<float>& centerPos) : centerPos_(centerPos), type_(Collider_Info::TYPE::NONE) {}
+
+const Vector2<float>& AbstractCollider::GetCenterPos(void) const {
+	return centerPos_;
+}
+
+void AbstractCollider::SetCenterPos(const Vector2<float>& centerPos) {
+	centerPos_ = centerPos;
+}
--- Source/GameObject/2D/Collider/AbstractCollider.h
+++ Source/GameObject/2D/Collider/AbstractCollider.h
@@ -13,6 +13,14 @@ public:
 	/// @param point 点の座標
 	/// @return 衝突してるかどうか
 	virtual bool IsContains(const Vector2<float>& point) const = 0;
+
+	/// @brief 中心座標を返す 
+	/// @return 座標
+	const Vector2<float>& GetCenterPos(void) const;
+
+	/// @brief 中心座標をセットする
+	/// @param pos 座標
+	void SetCenterPos(const Vector2<float>& centerPos);
 protected:
 	// 中心座標
 	Vector2<float> centerPos_;
